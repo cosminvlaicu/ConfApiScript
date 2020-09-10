4fbcc5ed615734987a43171b5eba0a634f70fb4b
Cosmin Vlaicu <cosmin.vlaicu@yahoo.com>
2020-09-11 01:56:36 +0300

    final

--- export.py
+++ export.py
@@ -11,63 +11,40 @@ def download_url(url = "http://localhost:1990/confluence/download/temp/Confluenc
         for chunk in r.iter_content(chunk_size=chunk_size):
             fd.write(chunk)
 
+def parse_json(headers):
+    s = str(headers)
+    s = s.replace("'", '"')
+    return json.loads(s)
 
-payload = {'os_username': 'admin', 'os_password': 'admin'}
 authentication_tuple = ('admin','admin')
 
-base_url = sys.argv[1]      # http://localhost:1990/confluence/
-key_list = sys.argv[2].split(',')       # ds
 
+base_url = sys.argv[1]
+key_list = sys.argv[2].split(',')
+
+custom_wait = 1
+if len(sys.argv) > 3:
+    custom_wait = float(sys.argv[3])
 
 for key in key_list:
-    # create request page
     request_page_url = base_url + "/rest/confapi/1/backup/export/" + key
 
-    print(request_page_url)
-
     response_request_page = requests.get(request_page_url,auth=authentication_tuple)
-    s = str(response_request_page.headers)
-
-    s = s.replace("'",'"')
-    js = json.loads(s)
-
+    js = parse_json(response_request_page.headers)
     queue_url = js['Location']
-    print(queue_url)
-    # queue_url = "http://localhost:1990/confluence/rest/confapi/1/backup/queue/a1282066-cc00-4530-a226-ffb390539d0e"
+    time.sleep(0.20*custom_wait)
 
-    time.sleep(1)
     response_get_queue = requests.get(queue_url,auth=authentication_tuple)
-    print(response_get_queue.headers)
-    s = str(response_get_queue.headers)
+    if not response_get_queue.ok:
+        print("No task found for the given UUID")
+        exit()
 
-    s = s.replace("'", '"')
-    js = json.loads(s)
+    js = parse_json(response_get_queue.headers)
     zip_url = js['Location']
+    time.sleep(0.15*custom_wait)
 
-    time.sleep(1)
     save_file_path = os.getcwd() + "/Confluence-space-export-" + key + ".xml.zip"
-    print(save_file_path)
-    time.sleep(10)
+    time.sleep(0.05*custom_wait)
     download_url(url = zip_url, save_path = save_file_path)
 
-# p = requests.get('http://localhost:1990/confluence/rest/confapi/1/backup/export/ds',auth=authentication_tuple)
-# r = requests.get('http://localhost:1990/confluence/rest/confapi/1/backup/queue/babd669f-6eb9-421a-b22d-2dcd9bd9dd06',auth=('admin','admin'))
-# q = requests.get('http://localhost:1990/confluence/download/temp/Confluence-space-export-121346-26.xml.zip',auth=('admin','admin'))
-# file = open("./dumps.txt","w+")
 
-
-# print(r.headers)
-# print(r.ok)
-# print(r)
-
-# print(q.headers)
-# print(q.ok)
-# print(q)
-#
-# file.write(q.text)
-#
-# time.sleep(3)
-#
-
-#
-# download_url()
